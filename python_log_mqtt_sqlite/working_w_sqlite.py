3bef5be152ac1e527f1de8ec9e368efbcd57260e
minhnt27 <minhnt27@gmail.com>
2020-05-06 23:30:22 +0700

    Update working_w_sqlite.py

--- python_log_mqtt_sqlite/working_w_sqlite.py
+++ python_log_mqtt_sqlite/working_w_sqlite.py
@@ -13,38 +13,47 @@ import json
 import sqlite3
 
 # SQLite DB Name
-DB_Name =  r"C:\Users\minhnt27\Downloads\sqlite\testDB.db"
+#DB_Name =  r"C:\Users\minhnt27\Downloads\sqlite\testDB.db"
+DB_Name =  r"D:\lab\sqlite\testDB.db"
 
 #===============================================================
 # Database Manager Class
 
 class DatabaseManager():
-	def __init__(self):
-		self.conn = sqlite3.connect(DB_Name)
-		self.conn.execute('pragma foreign_keys = on')
-		self.conn.commit()
-		self.cur = self.conn.cursor()
-		
-	def add_del_update_db_record(self, sql_query, args=()):
-		self.cur.execute(sql_query, args)
-		self.conn.commit()
-		return
+    def __init__(self):
+        self.conn = sqlite3.connect(DB_Name)
+        self.conn.execute('pragma foreign_keys = on')
+        self.conn.commit()
+        self.cur = self.conn.cursor()
 
-	def select_db_record(self, sql_query):
-		self.cur.execute(sql_query)
-		
-	def create_table(self, create_table_sql):
-		try:
-			self.cur.execute(create_table_sql)
-		except Error as e:
-			print(e)
-		
-	def __del__(self):
-		self.cur.close()
-		self.conn.close()
+    def add_del_update_db_record(self, sql_query, args=()):
+        self.cur.execute(sql_query, args)
+        self.conn.commit()
+        return
+
+    def select_db_record(self, sql_query):
+        self.cur.execute(sql_query)
+
+    def create_table(self, create_table_sql):
+        try:
+            self.cur.execute(create_table_sql)
+        except Exception as e:
+            print(e)
+            
+    def drop_table(self, drop_table_sql):
+        try:
+            self.cur.execute(drop_table_sql)
+        except Exception as e:
+            print(e)
+            
+    def __del__(self):
+        self.cur.close()
+        self.conn.close()
 
 #===============================================================
-# Functions to init Table
+# Functions to work with DB
+
+# Function to init Table
 def New_Table():
     sql_create_table = """ CREATE TABLE IF NOT EXISTS SensorData (
                                         Id integer PRIMARY KEY NOT NULL,
@@ -58,8 +67,12 @@ def New_Table():
     del dbObj
     print ("New table SensorData was created.")
 
-#===============================================================
-# Functions to push Sensor Data into Database
+def Drop_Table():
+    sql_drop_table = """ DROP TABLE IF NOT EXISTS SensorData; """
+    dbObj = DatabaseManager()
+    dbObj.drop_table(sql_drop_table)
+    del dbObj
+    print ("table SensorData was deleted.")
 
 # Function to save to DB Table
 def Sensor_Data_Loger(jsonData):
@@ -82,15 +95,11 @@ def Sensor_Data_Loger(jsonData):
 
 # Function to read DB Table
 def Sensor_Data_Read():
-	
-	#select data from DB Table
-	dbObj = DatabaseManager()
-	dbObj.select_db_record("select Date_n_Time, Message from SensorData")
-	rows = dbObj.fetchall()
-	for row in rows:
-        print(row)
-	del dbObj
-	print ("Inserted Sensor Data into Database.")
+    dbObj = DatabaseManager()
+    results=dbObj.select_db_record("select * from SensorData;")
+    rows = results.fetchall()
+    #fetchall() fetches all the rows of a query result. It returns all the rows as a list of tuples. An empty list is returned if there is no record to fetch
+    return rows
 
 
 #================================================================
@@ -98,4 +107,7 @@ def Sensor_Data_Read():
 New_Table()
 jsonData = '{"SensorID":"54321", "Date":"01/20/2020", "Topic":"test/topic", "Message":"message"	}';
 Sensor_Data_Loger(jsonData)
-Sensor_Data_Read()
+rows=Sensor_Data_Read()
+for row in rows:
+    print(row)
+Drop_Table()
