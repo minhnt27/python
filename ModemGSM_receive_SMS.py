33e062b7696652bb146e8c718fe7b1cdd59d3acb
minhnt27 <minhnt27@gmail.com>
2021-02-20 13:13:54 +0700

    ok

---
+++ ModemGSM_receive_SMS.py
@@ -0,0 +1,63 @@
+import serial
+import time
+import re
+from pprint import pprint
+class TextMessage:
+    def connectPhone(self):
+        self.ser = serial.Serial(port='COM4', baudrate=115200, bytesize=serial.SEVENBITS, parity=serial.PARITY_EVEN, stopbits=1, timeout=5, dsrdtr=True) #for mine this was ttyUSB0 but could be ttyUSB1 etc. good idea to runs ls usb and find out that way
+        time.sleep(1)
+
+    def list(self):
+        self.ser.write(b'ATZ\r')
+        self.ser.write(b'ATE0\r')
+        time.sleep(1)
+        self.ser.write(b'AT+CMGF=1\r')# put in textmode
+        time.sleep(1)
+        self.ser.write(b'''AT+CMGL="ALL"''' + b'\r') #fetch all sms's
+        list = self.ser.readlines()
+        result=[]
+        for msg in list:
+            print(msg.decode('utf8','ignore'))
+            if "+CMGL" in msg.decode('utf8','ignore'): #+CMGL looks for all SMS messages
+                result.append(re.findall("(?:^|,)(\"(?:[^\"]+|\"\")*\"|[^,]*)", msg.decode('utf8','ignore')))
+                #print(msg.decode('utf8','ignore'))
+        return result
+                
+    def read(self,idmsg):
+        self.ser.write(b'ATZ\r')
+        time.sleep(1)
+        self.ser.write(b'AT+CMGF=1\r')# put in textmode
+        time.sleep(1)
+        atcmd='AT+CMGR=' + str(idmsg) + '\r'
+        self.ser.write(atcmd.encode())
+        read = self.ser.readlines()
+        for msg in read:
+            if (msg != b"\r\n") and (msg != b"OK\r\n"):
+                print(msg)
+
+    def delAll(self):
+        self.ser.write(b'ATZ\r')
+        time.sleep(1)
+        self.ser.write(b'ATZ\r AT+CMGD=1,4\r')
+
+    def disconnectPhone(self):
+        self.ser.close()
+
+
+try:
+    sms = TextMessage()
+    sms.connectPhone()
+    result=sms.list()
+    pprint(result)
+    count=len(result)
+    if count>0:
+        lastmsg=result[count-1]
+        header=lastmsg[0]
+        #print(lastmsg)
+        #print(header)
+        idmsg=str(header).split(":")
+        sms.read(idmsg=idmsg[1].strip())
+    sms.delAll()
+finally:
+    if sms:
+        sms.disconnectPhone()
